c65e5ff6ed70c5f6bc689fe2ac29094ba71792e8
Fendotri <bhaulku@gmail.com>
2024-12-18 22:17:54 +0300

    new-project
    ilk proje gönderimi

---
+++ vite.config.ts
@@ -0,0 +1,51 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: '/',
+    root: path.resolve(__dirname, './multi_pages/'),
+    publicDir: path.resolve(__dirname, './public/'),
+    assetsInclude: ['*.vert', '*.frag', '*.glsl'],
+    build: {
+        outDir: path.resolve(__dirname, './cube_gunman'),
+        target: 'esnext',
+        sourcemap: true,
+        emptyOutDir: true,
+        minify: false,
+        assetsInlineLimit: 40960,
+        rollupOptions: {
+            input: {
+                index: path.resolve(__dirname, './multi_pages/index.html'),
+                game: path.resolve(__dirname, './multi_pages/game/index.html')
+            }
+        }
+    },
+    resolve: {
+        mainFields: ['module', 'jsnext:main', 'jsnext'],
+        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+        alias: {
+            '@src': path.resolve(__dirname, './src/'),
+            '@assets': path.resolve(__dirname, './assets/'),
+            '@gameplay': path.resolve(__dirname, './src/gameplay/')
+        }
+    },
+    plugins: [vue(), vueJsx()],
+    envDir: path.resolve(__dirname, './vite_envs/'),
+    envPrefix: 'VITE_',
+
+    css: {
+        modules: {
+            generateScopedName: "[local]_[hash:base64:5]",
+            hashPrefix: "prefix",
+            localsConvention: "dashes",
+        },
+        preprocessorOptions: {
+            scss: {
+                charset: false,
+            }
+        }
+    }
+})
